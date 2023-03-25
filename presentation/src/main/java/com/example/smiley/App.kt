d595032ac0741b18939a09cabe12cbd6dcaa585e
donghyeon0915 <dongdong0915_@naver.com>
2023-03-26 01:57:48 +0900

    chore: KakaoSdk 초기화 (#28)

--- presentation/src/main/java/com/example/smiley/App.kt
+++ presentation/src/main/java/com/example/smiley/App.kt
@@ -1,6 +1,7 @@
 package com.example.smiley
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 
 /**
  * @HiltAndroidApp
@@ -9,5 +10,21 @@ import android.app.Application
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        catchAllError()
+
+        //Kakao Sdk 초기화
+        KakaoSdk.init(this, BuildConfig.KAKAO_SDK_APPKEY)
+    }
+
+    /**
+     * 앱이 강제종료 되어도 에러 로그를 출력할 수 있도록 함
+     */
+    private fun catchAllError(){
+        try {
+            Thread.setDefaultUncaughtExceptionHandler { thread, ex -> ex.printStackTrace() }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 }
