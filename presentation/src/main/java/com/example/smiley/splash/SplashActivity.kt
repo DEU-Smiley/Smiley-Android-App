eab6ded910fef73f36a657b444aa0673ca7f797c
donghyeon0915 <donghyeon0915@gmail.com>
2023-03-27 18:25:40 +0900

    feat: 스플래시 액티비티 추가 (#36)

---
+++ presentation/src/main/java/com/example/smiley/splash/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.smiley.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.smiley.R
+import com.example.smiley.main.MainActivity
+import kotlinx.coroutines.*
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        splashLogo(3)
+    }
+
+    private fun splashLogo(sec:Long){
+        Handler().postDelayed(Runnable {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent) //intent 에 명시된 액티비티로 이동
+            finish() //현재 액티비티 종료
+        }, 1000L * sec)
+    }
+}
