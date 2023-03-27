c782fe51151ccf2875906e4d3d7f66f8432aaceb
donghyeon0915 <donghyeon0915@gmail.com>
2023-03-28 02:25:15 +0900

    refactor: 공통 확장 함수 추가 (#43)

---
+++ presentation/src/main/java/com/example/smiley/common/extension/ViewExt.kt
@@ -0,0 +1,9 @@
+package com.example.smiley.common.extension
+
+import android.view.View
+
+fun View.gone(){ visibility = View.GONE }
+
+fun View.visible(){ visibility = View.VISIBLE }
+
+fun View.invisible(){ visibility = View.INVISIBLE }
