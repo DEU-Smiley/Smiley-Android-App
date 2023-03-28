a95252e87953f627a102e2687300c17d0acdc344
donghyeon0915 <donghyeon0915@gmail.com>
2023-03-28 15:08:50 +0900

    refacotr: applicationContext를 Activity의 context로 수정 (#43)

--- presentation/src/main/java/com/example/smiley/common/extension/ActivityExt.kt
+++ presentation/src/main/java/com/example/smiley/common/extension/ActivityExt.kt
@@ -6,7 +6,7 @@ import com.example.smiley.onboarding.OnBoardingActivity
 
 
 fun<T> Activity.changeActivity(activity:Class<T>) {
-    val intent = Intent(applicationContext, activity)
+    val intent = Intent(this, activity)
     startActivity(intent) //intent 에 명시된 액티비티로 이동
     finish() //현재 액티비티 종료
 }
