401700a76b4be5207d69b4464f9ff10baf868cdf
donghyeon0915 <donghyeon0915@gmail.com>
2023-03-29 20:27:01 +0900

    refactor: 카카오 로그인 로직 viewModel로 분리 (#59)

--- presentation/src/main/java/com/example/smiley/login/LoginActivity.kt
+++ presentation/src/main/java/com/example/smiley/login/LoginActivity.kt
@@ -4,69 +4,69 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.example.smiley.R
 import com.example.smiley.common.extension.changeActivity
 import com.example.smiley.common.extension.showGenericAlertDialog
 import com.example.smiley.databinding.ActivityLoginBinding
-import com.example.smiley.main.MainActivity
+import com.example.smiley.login.viewmodel.LoginActivityState
+import com.example.smiley.login.viewmodel.LoginViewModel
 import com.example.smiley.permission.PermissionActivity
-import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.common.model.KakaoSdkError
-import com.kakao.sdk.common.util.Utility
-import com.kakao.sdk.user.UserApi
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var bind:ActivityLoginBinding
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         bind = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        kakaoLogin()
-    }
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
 
-    private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        if(error != null){
-            Log.e(TAG, "카카오 로그인 에러 $error")
-            showGenericAlertDialog("로그인에 실패했습니다.")
-        } else if (token != null) {
-            Log.d(TAG, "카카오 로그인 성공 ${token.accessToken}")
-            changeActivity(PermissionActivity::class.java)
-        }
+        observeLoginState()
+        addLoginBtnClickEvent()
     }
 
     /**
      * 카카오 로그인 요청 메소드
      */
-    private fun kakaoLogin(){
+    private fun addLoginBtnClickEvent(){
         bind.kakaoLoginBtn.setOnClickListener {
-            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오 계정으로 로그인
-            if(UserApiClient.instance.isKakaoTalkLoginAvailable(this)){
-                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                    if(error != null) {
-                        Log.e(TAG, "카카오 로그인 에러 $error")
-                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우
-                        // 의도적인 로그인 취소로 보고 카카오 계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                        if(error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                            return@loginWithKakaoTalk
-                        }
-
-                        // 카카오톡에 연결된 카카오 계정이 없는 경우, 카카오 계정으로 로그인 시도
-                        UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback)
-                    } else if (token != null) {
-                        Log.d(TAG, "카카오 로그인 성공 ${token.accessToken}")
-                        changeActivity(PermissionActivity::class.java)
-                    }
-                }
-            } else {
-                // 카카오톡이 설치되어 있지 않은 경우 이메일 로그인 방식으로 유도
-                UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback)
-            }
+            viewModel.kakaoLogin(this)
         }
+    }
+
+    private fun observeLoginState(){
+        viewModel.mState
+            .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+            .onEach { state -> handleLoginStateChange(state) }
+            .launchIn(lifecycleScope)
+    }
+
+    private fun handleLoginStateChange(state: LoginActivityState){
+        when(state){
+            is LoginActivityState.Init -> Unit
+            is LoginActivityState.SuccessLogin -> handleSuccessLogin()
+            is LoginActivityState.ErrorLogin -> handleErrorLogin(state.message)
+        }
+    }
+
+    private fun handleSuccessLogin(){
+        changeActivity(PermissionActivity::class.java)
+    }
+
+    private fun handleErrorLogin(message:String){
+        showGenericAlertDialog(message)
     }
 
     companion object{
