6fb869735a5d30972b8f05ae0df53b2ad0733e8b
donghyeon0915 <donghyeon0915@gmail.com>
2023-03-31 02:53:53 +0900

    feat: 버튼 클릭시 다음 뷰 보여주는 확장 함수 추가 (#49)

--- presentation/src/main/java/com/example/smiley/common/extension/ViewExt.kt
+++ presentation/src/main/java/com/example/smiley/common/extension/ViewExt.kt
@@ -1,8 +1,17 @@
 package com.example.smiley.common.extension
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.ScrollView
+import androidx.core.view.get
+import com.example.smiley.App
 import com.example.smiley.R
 
 fun View.gone(){ visibility = View.GONE }
@@ -19,4 +28,49 @@ fun Button.setDisabled(){
 fun Button.setEnabled(){
     this.setTextColor(resources.getColor(R.color.white))
     this.isEnabled = true
+}
+
+
+/**
+ * 선택된 라디오 버튼에 따라 다음 뷰를 표시하는 메소드
+ * 버튼 2개인 경우만 해당
+ * @param visibleView:View
+ * @param goneView:View?
+ */
+fun RadioGroup.showViewThenCheckedChanged(visibleView:View, goneView:View?) {
+    this.setOnCheckedChangeListener { _, checkedId ->
+        if(checkedId == this[0].id || goneView == null) visibleView.visible()
+        else {
+            visibleView.gone()
+            goneView.visible()
+        }
+    }
+}
+
+/**
+ * ScrollView를 맨 밑으로 내리는 메소드
+ */
+fun ScrollView.scrollDown(){
+    this.post{
+        this.fullScroll(ScrollView.FOCUS_DOWN)
+    }
+}
+
+
+/**
+ * EditText에서 엔터 누르면 다음 뷰를 표시하는 메소드
+ * @param nextView:View
+ * @param scrollView:ScrollView?
+ */
+fun EditText.showViewThenEnterPressed(nextView: View, scrollView: ScrollView?) {
+    setOnKeyListener { view, keyCode, _ ->
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val input = (view as EditText).text
+            if (input.isNotEmpty() && input.isNotBlank()) {
+                nextView.visible()
+                scrollView?.scrollDown()
+            }
+        }
+        false
+    }
 }
