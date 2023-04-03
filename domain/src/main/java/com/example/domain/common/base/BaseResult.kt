d5f7ed6779284916e436ccee7dc4cfc79fb03895
donghyeon0915 <donghyeon0915@gmail.com>
2023-04-04 03:41:05 +0900

    feat: 네트워크 요청/응답 관련 클래스 추가 (#77)

--- domain/src/main/java/com/example/domain/common/base/BaseResult.kt
+++ domain/src/main/java/com/example/domain/common/base/BaseResult.kt
@@ -1,4 +1,6 @@
 package com.example.domain.common.base
 
-class BaseResult {
+sealed class BaseResult <out T: Any, out U: Any> {
+    data class Success <T: Any>(val data: T): BaseResult<T, Nothing>()
+    data class Error <U: Any>(val rawResponse: U): BaseResult<Nothing, U>()
 }
