d7cc0300fc2af4ff2e93190f66eab748fa3d3349
donghyeon0915 <donghyeon0915@gmail.com>
2023-04-10 21:07:17 +0900

    feat: 커스텀 다이얼로그 생성 (#60)

---
+++ presentation/src/main/java/com/example/smiley/common/dialog/LoadingDialog.kt
@@ -0,0 +1,16 @@
+package com.example.smiley.common.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import com.example.smiley.R
+
+class LoadingDialog constructor(context:Context): Dialog(context) {
+
+    init {
+        setCanceledOnTouchOutside(false)
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        setContentView(R.layout.dialog_loading)
+    }
+}
