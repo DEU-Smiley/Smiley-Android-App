c4f01dbb6c6436b13b43428443a360b1d8679739
donghyeon0915 <donghyeon0915@gmail.com>
2023-04-10 16:52:22 +0900

    feat: Entity용 Mapper 클래스 추가, 기존 Mapper 클래스 수정 (#88)

---
+++ data/src/main/java/com/example/data/common/mapper/EntityMapper.kt
@@ -0,0 +1,13 @@
+package com.example.data.common.mapper
+
+/**
+ * Entity(data) -> BaseModel(domain)로 변경하는 인터페이스
+ * @param Entity:  내부 DB 테이블에 매핑된 Entity
+ * @param BaseModel: Domain 계층에서 사용하는 Dto
+ *
+ * @return BaseModel
+ */
+interface EntityMapper<From, To> {
+    fun From.mapToDomainModel(): To
+    fun To.mapFromDomainModel(): From
+}
