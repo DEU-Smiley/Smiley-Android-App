a0ae50946b13887a2e10a55f4e1e969bcdf863c2
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2023-04-30 23:23:40 +0900

    [Feat] : 커스텀 다이얼로그 디자인 수정 / 종류 추가 (#116)
    * feat: 다이얼로그 추가/수정 (#115)
    
    * feat: 확장 함수를 사용하는 클래스에 변경 사항 반영 (#115)
    
    * chore: 빌드 오류 수정 (#115)

--- presentation/src/main/java/com/example/smiley/common/dialog/LottieConfirmDialog.kt
+++ presentation/src/main/java/com/example/smiley/common/dialog/LottieConfirmDialog.kt
@@ -6,41 +6,32 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.RawRes
+import com.airbnb.lottie.LottieAnimationView
 import com.example.smiley.R
 import com.example.smiley.common.extension.gone
 
-class GenericDialog constructor(context: Context): Dialog(context) {
+class LottieConfirmDialog constructor(context: Context): Dialog(context) {
     private var confirmBtn : TextView
-    private var cancleBtn  : TextView
 
     init {
         setCanceledOnTouchOutside(false)
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        setContentView(R.layout.dialog_generic)
+        setContentView(R.layout.dialog_confirm_with_lottie)
 
         confirmBtn  = findViewById(R.id.dialog_confirm_btn)
-        cancleBtn   = findViewById(R.id.dialog_cancle_btn)
     }
 
-    /**
-     * 다이얼로그의 제목을 설정하는 메소드
-     */
     fun setTitle(title:String){
         val titleView = findViewById<TextView>(R.id.dialog_title)
         titleView.text = title
     }
 
-    /**
-     * 다이얼로그의 내용을 설정하는 메소드
-     */
     fun setContent(content:String){
         val contentView = findViewById<TextView>(R.id.dialog_content)
         contentView.text = content
     }
 
-    /**
-     * 다이얼로그의 부가 내용을 설정하는 메소드
-     */
     fun setSubContent(subContent:String){
         val subContentView = findViewById<TextView>(R.id.dialog_sub_content)
         if(subContent == "") subContentView.gone()
@@ -48,31 +39,12 @@ class GenericDialog constructor(context: Context): Dialog(context) {
         subContentView.text = subContent
     }
 
-    /**
-     * 확인 버튼의 텍스트를 변경하는 메소드
-     */
-    fun setConfirmBtnText(text:String){
-        confirmBtn.text = text
+    fun setLottieView(@RawRes rawRes:Int){
+        val lottieView = findViewById<LottieAnimationView>(R.id.lottie_view)
+        lottieView.setAnimation(rawRes)
     }
 
-    /**
-     * 취소 버튼의 텍스트를 변경하는 메소드
-     */
-    fun setCancleBtnText(text:String){
-        cancleBtn.text = text
-    }
-
-    /**
-     * 다이얼로그의 확인 버튼 클릭 이벤트 등록 메소드
-     */
     fun setConfirmBtnClickListener(listner: View.OnClickListener) {
         this.confirmBtn.setOnClickListener(listner)
-    }
-
-    /**
-     * 다이얼로그의 취소 버튼 클릭 이벤트 등록 메소드
-     */
-    fun setCancleBtnClickListener(listner: View.OnClickListener) {
-        this.cancleBtn.setOnClickListener(listner)
     }
 }
--- presentation/src/main/java/com/example/smiley/common/dialog/LottieGenericDialog.kt
+++ presentation/src/main/java/com/example/smiley/common/dialog/LottieGenericDialog.kt
@@ -6,17 +6,19 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.RawRes
+import com.airbnb.lottie.LottieAnimationView
 import com.example.smiley.R
 import com.example.smiley.common.extension.gone
 
-class GenericDialog constructor(context: Context): Dialog(context) {
+class LottieGenericDialog constructor(context: Context): Dialog(context) {
     private var confirmBtn : TextView
     private var cancleBtn  : TextView
 
     init {
         setCanceledOnTouchOutside(false)
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        setContentView(R.layout.dialog_generic)
+        setContentView(R.layout.dialog_generic_with_lottie)
 
         confirmBtn  = findViewById(R.id.dialog_confirm_btn)
         cancleBtn   = findViewById(R.id.dialog_cancle_btn)
@@ -46,6 +48,14 @@ class GenericDialog constructor(context: Context): Dialog(context) {
         if(subContent == "") subContentView.gone()
 
         subContentView.text = subContent
+    }
+
+    /**
+     * 다이얼로그의 LottieView에 애니메이션을 지정하는 메소드
+     */
+    fun setLottieView(@RawRes rawRes:Int){
+        val lottieView = findViewById<LottieAnimationView>(R.id.lottie_view)
+        lottieView.setAnimation(rawRes)
     }
 
     /**
