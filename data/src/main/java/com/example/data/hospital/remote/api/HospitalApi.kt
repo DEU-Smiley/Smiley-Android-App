6abb88e43ef22b4e60b10152f064c57a3617f371
donghyeon0915 <dongdong0915_@naver.com>
2023-05-10 15:59:03 +0900

    feat: 주변 병원 조회 Api 작성 (#127)

--- data/src/main/java/com/example/data/hospital/remote/api/HospitalApi.kt
+++ data/src/main/java/com/example/data/hospital/remote/api/HospitalApi.kt
@@ -1,9 +1,14 @@
 package com.example.data.hospital.remote.api
 
+import com.example.data.hospital.remote.request.NearbyHospitalRequest
+import com.example.data.hospital.remote.response.HospitalPositListResponse
 import com.example.data.hospital.remote.response.HospitalResponse
 import com.example.data.hospital.remote.response.SimpleHospitalListResponse
+import com.example.domain.hospital.model.SimpleHospitalList
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 internal interface HospitalApi {
@@ -13,4 +18,7 @@ internal interface HospitalApi {
 
     @GET("hospitals/{hpid}")
     suspend fun getHospitalByHpid(@Path("hpid") hpid:String): Response<HospitalResponse>
+
+    @POST("hospitals/nearhospitalinfos")
+    suspend fun getNearbyHospital(@Body nearbyHospitalRequest: NearbyHospitalRequest): Response<HospitalPositListResponse>
 }
