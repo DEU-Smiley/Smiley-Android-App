55545706cfad8a4ec7b3cabb56838c14d5573d21
donghyeon0915 <donghyeon0915@gmail.com>
2023-05-11 16:27:30 +0900

    chore: String(yyMMdd) to LocalDate 확장 함수 추가(#112)

--- presentation/src/main/java/com/example/smiley/common/extension/DateExt.kt
+++ presentation/src/main/java/com/example/smiley/common/extension/DateExt.kt
@@ -1,5 +1,6 @@
 package com.example.smiley.common.extension
 
+import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -8,6 +9,7 @@ import java.util.*
 /**
  * @return String of "yyyy-mm-dd"
  */
+@SuppressLint("SimpleDateFormat")
 fun Date.toDateString(): String {
     return SimpleDateFormat("yyyy-mm-dd").format(this)
 }
@@ -24,6 +26,29 @@ fun String.toDateString(): String {
  */
 fun String.toDate(): LocalDate {
     return LocalDate.parse(this, DateTimeFormatter.ISO_DATE)
+}
+
+/**
+ * yyMMdd To yyyy-MM-dd
+ * @return LocalDate "yyyy-MM-dd"
+ */
+fun String.toDateOfyyMMdd(): LocalDate {
+    val twoDigitYear = this.substring(0, 2).toInt()
+    val year =
+        if (twoDigitYear in 0..99) {
+            if (twoDigitYear in 0..22) {
+                2000 + twoDigitYear
+            } else {
+                1900 + twoDigitYear
+            }
+        } else {
+            throw IllegalArgumentException("Invalid year format")
+        }
+
+    val month = this.substring(2, 4).toInt()
+    val day = this.substring(4, 6).toInt()
+
+    return LocalDate.of(year, month, day)
 }
 
 fun Calendar.getTodayOfWeek(): Int {
