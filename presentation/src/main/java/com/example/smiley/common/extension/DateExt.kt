6ae92970dd95a4704e22dd23fa54abbcbb6e4410
donghyeon0915 <dongdong0915_@naver.com>
2023-05-22 17:54:39 +0900

    feat: 날짜 차이 계산 확장 함수 추가 (#142)

--- presentation/src/main/java/com/example/smiley/common/extension/DateExt.kt
+++ presentation/src/main/java/com/example/smiley/common/extension/DateExt.kt
@@ -2,7 +2,10 @@ package com.example.smiley.common.extension
 
 import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
+import java.time.Duration
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Period
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -53,4 +56,11 @@ fun String.toDateOfyyMMdd(): LocalDate {
 
 fun Calendar.getTodayOfWeek(): Int {
     return this.get(Calendar.DAY_OF_WEEK)
+}
+
+/**
+ * 두 날짜의 일 수 차이를 반환하는 메소드
+ */
+fun LocalDateTime.getDateDifference(target: LocalDateTime): Int {
+    return Period.between(target.toLocalDate(), this.toLocalDate()).days
 }
