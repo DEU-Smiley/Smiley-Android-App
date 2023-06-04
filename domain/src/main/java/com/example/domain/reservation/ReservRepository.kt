1fbfd5dda8aa6509d2c33df53d352ee554a4041b
Donghyeon0915 <donghyeon0915@gmail.com>
2023-06-04 21:14:41 +0900

    feat: 예약 Domain 레이어 구현(#154)

--- domain/src/main/java/com/example/domain/reservation/ReservRepository.kt
+++ domain/src/main/java/com/example/domain/reservation/ReservRepository.kt
@@ -1,4 +1,10 @@
-package com.example.domain.reservation.model
+package com.example.domain.reservation
+
+import com.example.domain.common.base.ResponseState
+import com.example.domain.reservation.model.ReservList
+import kotlinx.coroutines.flow.Flow
+
 
 interface ReservRepository {
+    suspend fun getReservHistory(): Flow<ResponseState<ReservList>>
 }
--- domain/src/main/java/com/example/domain/reservation/usecase/GetReservHistoryUseCase.kt
+++ domain/src/main/java/com/example/domain/reservation/usecase/GetReservHistoryUseCase.kt
@@ -1,4 +1,17 @@
 package com.example.domain.reservation.usecase
 
-class GetReservHistoryUseCase {
+import com.example.domain.common.base.ResponseState
+import com.example.domain.reservation.ReservRepository
+import com.example.domain.reservation.model.ReservList
+import dagger.Reusable
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@Reusable
+class GetReservHistoryUseCase @Inject constructor(
+    private val reservRepository: ReservRepository
+) {
+    suspend operator fun invoke(): Flow<ResponseState<ReservList>> {
+        return reservRepository.getReservHistory()
+    }
 }
