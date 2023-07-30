786ffd569cac090d6aaa01db61220965b77d979a
Donghyeon-Kim <dongdong99@kakao.com>
2023-07-30 17:44:54 +0900

    feat: 로그인 화면 디자인 변경(#169)

--- presentation/src/main/java/com/example/smiley/login/LoginActivity.kt
+++ presentation/src/main/java/com/example/smiley/login/LoginActivity.kt
@@ -1,14 +1,26 @@
 package com.example.smiley.login
 
+import android.graphics.Color
+import android.graphics.SurfaceTexture
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.Surface
+import android.view.TextureView
+import android.view.View
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.datastore.dataStore
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.example.smiley.R
 import com.example.smiley.common.extension.changeActivity
+import com.example.smiley.common.extension.setTransparentStatusBarAndNavigationBar
 import com.example.smiley.common.extension.showConfirmDialog
 import com.example.smiley.common.extension.showGenericAlertDialog
 import com.example.smiley.databinding.ActivityLoginBinding
@@ -23,11 +35,14 @@ import com.kakao.sdk.user.model.User
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import java.io.IOException
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     private lateinit var bind: ActivityLoginBinding
     private val viewModel: LoginViewModel by viewModels()
+
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,16 +50,22 @@ class LoginActivity : AppCompatActivity() {
         bind = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         // 카카오 자동 로그인 체크
+        initLoginVideoView()
         checkKakaoAccessToken()
         observeLoginState()
         addLoginBtnClickEvent()
+    }
+
+    private fun initLoginVideoView(){
+        bind.ttvLoginVideo.surfaceTextureListener = surfaceTextureListener
+        this.setTransparentStatusBarAndNavigationBar()
     }
 
     /**
      * 카카오 로그인 요청 메소드
      */
     private fun addLoginBtnClickEvent() {
-        bind.kakaoLoginBtn.setOnClickListener {
+        bind.clKakaoLoginBtn.setOnClickListener {
             viewModel.kakaoLogin(this)
         }
     }
@@ -96,6 +117,33 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+            val video: Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.video_login)
+
+            mediaPlayer = MediaPlayer().apply {
+                setDataSource(this@LoginActivity, video)
+                setSurface(Surface(surface))
+                prepareAsync()
+                isLooping = true
+
+                setOnPreparedListener {
+                    this.start()
+                }
+            }
+        }
+        override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) = Unit
+        override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean = false
+        override fun onSurfaceTextureUpdated(p0: SurfaceTexture) = Unit
     }
 
     companion object {
