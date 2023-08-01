beeb80de1e11ceac9a327d511c8f253209ba8871
Donghyeon-Kim <dongdong99@kakao.com>
2023-08-01 21:25:19 +0900

    refactor: ErrorResponse 초기값 변경(#169)

--- data/src/main/java/com/example/data/common/network/ErrorResponse.kt
+++ data/src/main/java/com/example/data/common/network/ErrorResponse.kt
@@ -2,6 +2,7 @@ package com.example.data.common.network
 
 import com.example.data.common.mapper.DataMapper
 import com.example.domain.common.base.NetworkError
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -14,9 +15,11 @@ class ErrorResponse(
 ) : BaseResponse {
     companion object: DataMapper<ErrorResponse, NetworkError> {
         override fun ErrorResponse.toDomainModel(): NetworkError {
+            // 서버측에서 status와 code가 반대로 오고 있어 바꿔서 매핑
             return NetworkError(
-                error = error ?: "null",
-                code = status ?: "null",
+                error = error ?: "NO_ERROR",
+                code = code ?: "NO_CODE",
+                status  = status ?: "NO_STATUS",
                 message = message ?: "알 수 없는 에러"
             )
         }
