ded13b35b306b29a9033f996b9fd323041166419
Donghyeon-Kim <donghyeon0915@gmail.com>
2023-08-20 21:44:39 +0900

    feat: 커스텀 착용 시간 그래프 메소드 추가(#183)

--- presentation/src/main/java/com/example/smiley/common/customview/CustomWearingProgressBar.kt
+++ presentation/src/main/java/com/example/smiley/common/customview/CustomWearingProgressBar.kt
@@ -6,12 +6,15 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.animation.doOnEnd
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.smiley.R
 import com.example.smiley.common.extension.dpToPx
+import com.example.smiley.common.extension.invisible
+import com.example.smiley.common.extension.visible
 import com.example.smiley.databinding.CustomEmojiProgressBarBinding
 import com.example.smiley.databinding.CustomWearingProgressBarBinding
 
@@ -45,11 +48,14 @@ class CustomWearingProgressBar @JvmOverloads constructor(
             updateProgress(progressFromXml)
         }
         setMax(maxFromXml)
-        // updateProgressTint(progressTintXml)
-
         attributes.recycle()
     }
 
+    /**
+     * ProgressBar의 진행률을 업데이트하는 메소드 (+ 애니메이션)
+     *
+     * @param progress Int
+     */
     private fun updateProgress(progress: Int) {
         progressBar.progress = progress
         val animator = ObjectAnimator.ofInt(progressBar, "progress", 0, progressBar.progress)
@@ -68,5 +74,39 @@ class CustomWearingProgressBar @JvmOverloads constructor(
 
     private fun setMax(max: Int){
         progressBar.max = max
+    }
+
+    /**
+     * 그래프의 세로 구분선의 표시 여부를 지정하는 메소드
+     *
+     * @param isShow Boolean
+     */
+    fun showDevideLine(isShow: Boolean){
+        val divides = arrayListOf(
+            bind.view1, bind.view2, bind.view3, bind.view4, bind.view5, bind.view6
+        )
+
+        if(isShow){
+            divides.forEach { it.visible() }
+        } else {
+            divides.forEach { it.invisible() }
+        }
+    }
+
+    /**
+     * 그래프의 퍼센트 라벨 표시 여부를 지정하는 메소드
+     *
+     * @param isShow Boolean
+     */
+    fun showPercentLabel(isShow: Boolean){
+        val labels = arrayListOf(
+            bind.tvPercent20, bind.tvPercent40, bind.tvPercent70, bind.tvPercent100
+        )
+
+        if(isShow){
+            labels.forEach { it.visible() }
+        } else {
+            labels.forEach { it.invisible() }
+        }
     }
 }
