21c6d744015026e60bcb04fd0dfe9666cf796fe2
Donghyeon-Kim <donghyeon0915@gmail.com>
2023-11-15 02:01:35 +0900

    feat: 데코레이터 추가 (#193)

--- presentation/src/main/java/com/example/smiley/common/utils/decorutils/EdgeSpaceDecoration.kt
+++ presentation/src/main/java/com/example/smiley/common/utils/decorutils/EdgeSpaceDecoration.kt
@@ -7,15 +7,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.smiley.common.extension.dpToPx
 
 /**
- * RecyclerView 아이템 사이의 간격을 설정하는 클래스
+ * RecyclerView의 양 끝의 여백을 지정하는 클래스
  * @param context Context
  * @param dp Int
- * @param isEdgeSpacingEnabled Boolean - 양 끝의 아이템에도 여백을 적용할 지 여부
+ * @param isFirstTopPadding Boolean 상단에 여백을 적용할 지의 여부
+ * @param isLastBottomPadding Boolean 하단에 역배을 적용할 지의 여부
  */
-class SpaceItemDecoration(
+class EdgeSpaceDecoration(
     private val context: Context,
     private val dp: Int,
-    private val isEdgeSpacingEnabled: Boolean = false
+    private val isFirstTopPadding: Boolean = false,
+    private val isLastBottomPadding: Boolean = false
 ) : RecyclerView.ItemDecoration() {
     private val pixel = dp.dpToPx(context)
 
@@ -26,10 +28,14 @@ class SpaceItemDecoration(
         state: RecyclerView.State
     ) {
         val position = parent.getChildAdapterPosition(view)
+        val isFirst = position == 0
+        val isLast = position == (parent.adapter?.itemCount?: 0)-1
 
-        if (isEdgeSpacingEnabled || position != 0) {
-            outRect.left = pixel
+        if (isFirstTopPadding && isFirst) {
+            outRect.top = pixel
         }
-        outRect.right = pixel
+        if(isLastBottomPadding && isLast){
+            outRect.bottom = pixel
+        }
     }
 }
